e82d7ad0e6e070b3224b1b5dff539b9cb6975758
NikolaySviridok <48551929+NikolaySviridok@users.noreply.github.com>
2022-11-13 14:22:13 +0700

    Add files via upload

---
+++ 4.1.java
@@ -0,0 +1,22 @@
+public class home1 {
+    // Пусть дан LinkedList с несколькими элементами. Реализуйте метод, который вернет “перевернутый” список.
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(i);
+        }
+        System.out.println(list);
+        System.out.println(reverseList(list));
+        // Collections.reverse(list);
+        // System.out.println(list);
+    }
+
+    static LinkedList<Integer> reverseList(LinkedList<Integer> list) {
+        LinkedList<Integer> nlist = new LinkedList<>();
+        int length = list.size();
+        for (int i = 0; i < length; i++) {
+            nlist.add(list.pollLast());
+        }
+        return nlist;
+    }
+}
